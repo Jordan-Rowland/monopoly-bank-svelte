25fb897b939f0886532811d86cdb0630f5c99866
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2020-01-11 17:06:54 -0800

    integrates move history menu

---
+++ src/LastMove.svelte
@@ -0,0 +1,38 @@
+<script>
+
+export let message;
+
+</script>
+
+<div on:click>
+  <span>
+    {message}
+  </span>
+</div>
+
+<style>
+
+div {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 35px;
+  width: 100%;
+  top: 0;
+  left: 0;
+  padding: 15px;
+  background-color: hsl(10, 50%, 70%);
+  color: hsl(0, 0%, 99%);
+  font-weight: 800;
+}
+
+
+@media only screen and (max-width: 640px) {
+  div {
+    font-size: 0.8rem;
+  }
+}
+
+
+</style>
