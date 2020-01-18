f7470b3bb57af40577d20b7603112cd350debd98
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2020-01-18 13:58:12 -0800

    new build, updates service worker to v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 1;
+const CACHE_VERSION = 2;
 const CACHE_STATIC_NAME  = `static-v${CACHE_VERSION}`;
 const CACHE_DYNAMIC_NAME  = `dynamic-v${CACHE_VERSION}`;
 const STATIC_FILES = [
@@ -13,19 +13,21 @@ const STATIC_FILES = [
   './manifest.webmanifest'
 ];
 
+
 // Cleaning up cache
 function trimCache(cacheName, maxItems) {
   caches.open(cacheName)
-  .then(cache => {
-    cache.keys()
-    .then(keys => {
-    if (keys.length > maxItems) {
-      cache.delete(keys[0])
-      .then(trimCache(cacheName, maxItems));
-    }
+    .then(cache => {
+      cache.keys()
+      .then(keys => {
+        if (keys.length > maxItems) {
+          cache.delete(keys[0])
+          .then(trimCache(cacheName, maxItems));
+        }
+    });
   });
-  })
 }
+
 
 // Cache app shell
 self.addEventListener('install', event => {
@@ -36,8 +38,9 @@ self.addEventListener('install', event => {
       console.log(`[Service Worker] Pre-caching app shell -> ${CACHE_STATIC_NAME}`);
       cache.addAll(STATIC_FILES);
     })
-  )
+  );
 });
+
 
 self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating service worker', event);
@@ -47,7 +50,7 @@ self.addEventListener('activate', event => {
     .then(function(key_list) {
       return Promise.all(key_list.map(function(key) {
         if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-          console.log(`[Service Worker] Removing old cache -> ${key}`)
+          console.log(`[Service Worker] Removing old cache -> ${key}`);
           return caches.delete(key);
         }
       }));
@@ -70,21 +73,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
-
-// // Network with cache fallback strategy
-// self.addEventListener('fetch', function(event) {
-//   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-//   return;
-//   }
-//   event.respondWith(
-//     fetch(event.request)
-//      .then(res => {
-//       return caches.open(CACHE_DYNAMIC_NAME)
-//         .then(cache => {
-//           cache.put(event.request.url, res.clone());
-//           return res;
-//         });
-//     })
-//   );
-// });
